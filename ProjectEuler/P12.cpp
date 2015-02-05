da7f44409a046b3a9cb063a98c0529f18f3ea4ca
Benevolous <jcsizemo@gmail.com>
2015-02-05 09:13:30 -0500

    Finished 12, on 13

--- ProjectEuler/P12.cpp
+++ ProjectEuler/P12.cpp
@@ -1,39 +1,27 @@
 #include <iostream>
 #include <vector>
-#include "Primes.h"
-
-const int size = 10000000;
-
-std::vector<int> factors(size);
-std::vector<int> primes;
+#include <cmath>
 
 int getFactors(int n) {
 
-	if (n == 1) return 0;
-	if (factors[n] != factors[n]) return factors[n];
-
-	int index = 0;
-	int prime = 2;
-
-	while (n != prime && n % prime != 0) prime = primes[++index];
-
-	if (n % prime == 0) {
-		factors[n] = 2 + getFactors(n / prime);
-		return factors[n];
+	int factors = 2;
+	double root = sqrt(n);
+	if (root == n/root) factors--;
+	for (int i = 2; i <= (int) root; i++) {
+		if (n % i == 0) factors += 2;
 	}
+
+	return factors;
 }
 
 int p12() {
 
-	int i = 2;
-	std::cout << "one" << std::endl;
-	primes = getPrimes(size);
-	std::cout << "two" << std::endl;
+	int tri = 0;
 
-	for (int numDivisors = 0; numDivisors < 500; i++) {
-		int tri = i*(i + 1) / 2;
+	for (int i = 2, numDivisors = 0; numDivisors < 500; i++) {
+		tri = i*(i + 1) / 2;
 		numDivisors = getFactors(tri);
 	}
 
-	return i * (i+1) / 2;
+	return tri;
 }
