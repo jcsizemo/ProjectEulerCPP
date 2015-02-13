40f0e1a874c4ad2c30ea369776fc397126d66843
John <jcsizemo@gmail.com>
2015-02-13 15:23:40 -0500

    Finished up through problem 22

--- ProjectEuler/P22.cpp
+++ ProjectEuler/P22.cpp
@@ -1,14 +1,35 @@
-#include "Primes.h"
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
 int p22() {
 
-	int primeCount = 0;
-	for (int i = 2; i <= 100000; i++) {
-		if (isPrime(i)) {
-			std::cout << "Prime #" << ++primeCount << ": " << i << std::endl;
-		}
+	std::ifstream input("p022_names.txt");
+	std::string value;
+	std::vector<std::string> names;
+
+	while (std::getline(input, value, ',')) {
+		value.erase(std::remove(value.begin(), value.end(), '\"'), value.end());
+		names.push_back(value);
 	}
 
-	return 0;
+	std::sort(names.begin(), names.end());
+	int scoreSum = 0;
+
+	for (int i = 0; i < (int)names.size(); i++) {
+
+		std::string name = names[i];
+		int score = 0;
+
+		for (int j = 0; j < (int)name.size(); j++) score += ((int)name[j]) - 64;
+
+		scoreSum += score * (i + 1);
+	}
+
+	input.close();
+
+	return scoreSum;
 }
